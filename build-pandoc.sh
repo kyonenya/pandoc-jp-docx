ce8a0187af1fe497b6a3eac4bb21a367e063460f
kyonenya <kyon.musica@gmail.com>
2026-05-25 22:52:27 +0900

    feat: input として glob pattern を受け付けるようにする
    ネストされたフォルダもコンパイルできるように

--- build-pandoc.sh
+++ build-pandoc.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 set -eu
 
-input_dir=${1:?Usage: $0 input_dir output_file [config]}
-output_file=${2:?Usage: $0 input_dir output_file [config]}
+input_pattern=${1:?Usage: $0 input_pattern output_file [config]}
+output_file=${2:?Usage: $0 input_pattern output_file [config]}
 config=${3:-}
 
 if [ "${GITHUB_ACTIONS:-}" = "true" ]; then
-  input_dir="caller/$input_dir"
+  input_pattern="caller/$input_pattern"
   output_file="caller/$output_file"
   config=${config:+"caller/$config"}
 fi
 
 mkdir -p "$(dirname "$output_file")"
 
+set -- $input_pattern  # expand
+
+if [ "$#" -eq 1 ] && [ "$1" = "$input_pattern" ]; then
+  echo "No input files matched: $input_pattern" >&2
+  exit 1
+fi
+
 pandoc \
   --defaults=defaults.yml \
   --output="$output_file" \
   ${config:+--defaults="$config"} \
-  "$input_dir"/[0-9]*.md
+  "$@"
